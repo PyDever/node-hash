1d6107547b692d9af0b99521528ae3b5f7e24202
Mellanie Soriano <23709723+mellaniesoriano@users.noreply.github.com>
2018-09-18 15:06:52 -1000

    Update KRH.js
    added comments and changed the starting hash value

--- src/KRH.js
+++ src/KRH.js
@@ -1,26 +1,31 @@
 
 /*
 Kaleb R. Horvath's Algorithm
-(Borrowing constants from DJB2 (5381...33))
+(Borrowing constants from DJB2 (5831...33))
+*/
+
+/*
+100 & 000 = 000
+100 & 100 = 100
+OR
+100 | 100 = 111
+101 | 101 = 111
 */
 
 var krh = function (str) {
 
-	var hash 	= 5381;
+	var hash = 5831;
 
 	for (var i = 0; i < str.length; ++i) {
-
 		/* extract first 8-bits from hash.
 		because 32-bits is max, make sure to extend
 		0xFF to 0xFF000000 to 
 		grab first (lowest) 8-bits */
 		var low_8 = hash & 0xFF000000; 			// 0x000000FF = highest 8-bits
-									   			// 0xFFFF0000 = lowest 16-bits
-									   			// 0x0000FFFF = highest 16-bits
-
-		hash = 33 * hash ^ str.charCodeAt(i); 	// DJB2..? wait, theres more
-		hash = hash ^ low_8;		  	// XOR by lowest 8-bits
-
+								// 0xFFFF0000 = lowest 16-bits
+								// 0x0000FFFF = highest 16-bits
+		hash = 33 * hash ^ str.charCodeAt(i); 		// DJB2..? wait, theres more
+		hash = hash ^ low_8;		  		// XOR by lowest 8-bits
 	}
 	return hash;
 }
